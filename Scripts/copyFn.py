ee9516d3ed41db5862b9f810ef892fe28ef2503b
RB <ranganathbalaji252@gmail.com>
2022-12-27 12:58:20 +0530

    Updated copyFn & deleteFn files with print statements

--- Scripts/copyFn.py
+++ Scripts/copyFn.py
@@ -5,6 +5,7 @@ import sys
 server = variables.server
 
 def copyJob(sourceJob,destinationJob):
+    print("Job Copy In-Progress")
     server.copy_job(sourceJob,destinationJob)
 
 copyJob(*sys.argv[1:])
--- Scripts/deleteFn.py
+++ Scripts/deleteFn.py
@@ -5,6 +5,7 @@ import sys
 server = variables.server
 
 def deleteJob(deleteJobName):
+    print("Deletion In-Progress")
     server.delete_job(deleteJobName)
 
 deleteJob(*sys.argv[1:])
